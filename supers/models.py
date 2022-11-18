850f2eb5b3db3f003564c13902962f143f091a1b
Mara Green <maragreen42@gmail.com>
2022-11-17 21:15:24 -0800

    completed building tables and migrated to my SQL Workbench

--- supers/models.py
+++ supers/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from super_types.models import SuperType
 # Create your models here.
 
 class Super(models.Model):
@@ -8,5 +8,4 @@ class Super(models.Model):
     primary_ability = models.CharField(max_length=255)
     secondary_ability = models.CharField(max_length=255)
     catchphrase = models.CharField(max_length=255)
-    super_type = models.ForeignKey()
-    
+    super_type = models.ForeignKey(SuperType, models.CASCADE)
