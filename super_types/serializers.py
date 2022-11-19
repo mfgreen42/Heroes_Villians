0875e501544a2b8307265ba6cb2d1625e2886648
Mara Green <maragreen42@gmail.com>
2022-11-18 17:38:19 -0800

    created supers_list function in Views file, get all

---
+++ super_types/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import SuperType
+
+
+class SuperTypeSerializer(serializers.ModelSerializer):
+    model = SuperType
+    fields = ['id','type']
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Super
+from .models import SuperType
+from . serializers import SupersSerializer
+from super_types.serializers import SuperTypeSerializer
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+@api_view(['GET'])
+def supers_list(requst):
+    if requst.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SupersSerializer(supers, many = True)
+        return Response(serializer.data)
